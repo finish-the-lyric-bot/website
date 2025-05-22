2b8da4c0e6687f017bcef2ef50d1c15fb21c947c
MyferIsADev <contactme.myfer@protonmail.com>
2025-05-22 18:44:34 -0500

    use `Footer.svelte` component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-	import Cta from '../lib/components/CTA.svelte';
+	import Footer from '../lib/components/Footer.svelte';
 	import Features from '../lib/components/Features.svelte';
 	import Hero from '../lib/components/Hero.svelte';
 </script>
 
 <Hero />
 <Features />
-<Cta />
+<Footer />
